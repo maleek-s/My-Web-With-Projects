67a84a37242f051bbf25c5e031f38256f7027633
maleek-s <malik1@windowslive.com>
2022-10-18 20:27:16 +0200

    added the basic structure, first actual commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import { generatePalette } from "./colorHelpers";
 import Palette from "./Palette";
-import seedColors  from "./seedColors";
+import seedColors from "./seedColors";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette palette={seedColors[4]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
